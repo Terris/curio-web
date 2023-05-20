6101f9c97a11cddff541f9eb601fef603a9b7b25
Terris Kremer <terris.kremer@gmail.com>
2023-05-20 06:07:57 -0400

    update db column names

---
+++ prisma/migrations/20230520094838_dn_naming_conventions/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `access_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `expires_at` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `id_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refresh_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `session_state` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `token_type` on the `Account` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "access_token",
+DROP COLUMN "expires_at",
+DROP COLUMN "id_token",
+DROP COLUMN "refresh_token",
+DROP COLUMN "session_state",
+DROP COLUMN "token_type",
+ADD COLUMN     "accessToken" TEXT,
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "expiresAt" INTEGER,
+ADD COLUMN     "idToken" TEXT,
+ADD COLUMN     "refreshToken" TEXT,
+ADD COLUMN     "sessionState" TEXT,
+ADD COLUMN     "tokenType" TEXT;
+
+-- AlterTable
+ALTER TABLE "Session" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "VerificationToken" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
