827eec5306274e2d2892ea8feb67e99cb8969771
Terris Kremer <terris.kremer@gmail.com>
2023-05-26 22:30:15 -0400

    remove stupid hero text

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,15 +2,7 @@ import { getAdminLayout } from "@/layout/AdminLayout";
 import { Text } from "@/ui";
 
 export default function AdminPage() {
-  return (
-    <>
-      <Text size={2}>
-        Transform your experiences into captivating stories, preserving the
-        essence of your journey in a single, remarkable platform. Let your story
-        unfold, one chapter at a time.
-      </Text>
-    </>
-  );
+  return <></>;
 }
 
 AdminPage.getLayout = getAdminLayout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
 import { getPublicLayout } from "@/layout/PublicLayout";
 import { Text, styled } from "@/ui";
 export default function HomePage() {
-  return (
-    <Hero>
-      <Text size={3}>
-        Transform your experiences into captivating stories, preserving the
-        essence of your journey in a single, remarkable platform. Let your story
-        unfold, one chapter at a time.
-      </Text>
-    </Hero>
-  );
+  return <Hero></Hero>;
 }
 
 HomePage.getLayout = getPublicLayout;
